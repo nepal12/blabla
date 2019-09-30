8c39b460145d1350837df8be9726c83ed74dab3c
nepal12 <riteshrijal@yahoo.com>
2019-09-30 18:02:33 +0200

    4-5 complete

--- test/main_test.dart
+++ test/main_test.dart
@@ -1,12 +1,13 @@
-import 'package:blabla/main.dart';
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:blabla/main.dart';
+import 'package:blabla/pages/ConversationPageList.dart';
 
 void main() {
-  testWidgets('Checking if hello world shows up', (WidgetTester tester) async {
+  testWidgets('Main UI Test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(Blabla());
-    expect(find.text('Hello World!'), findsOneWidget);
+
+    expect(find.byType(ConversationPageList),findsOneWidget);
+
   });
 }
