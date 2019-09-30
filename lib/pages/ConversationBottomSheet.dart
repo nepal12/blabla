7750d1895a729dd37736e89579101d2998146a72
nepal12 <riteshrijal@yahoo.com>
2019-09-30 18:37:14 +0200

    8 complete

--- lib/pages/ConversationBottomSheet.dart
+++ lib/pages/ConversationBottomSheet.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:blabla/config/Palette.dart';
 import 'package:blabla/config/Styles.dart';
 import 'package:blabla/widgets/ChatRowWidget.dart';
+
 import 'package:blabla/widgets/NavigationPillWidget.dart';
 
 class ConversationBottomSheet extends StatefulWidget {
@@ -19,17 +20,31 @@ class _ConversationBottomSheetState extends State<ConversationBottomSheet> {
         child: Scaffold(
             backgroundColor: Colors.white,
             body: ListView(children: <Widget>[
-              NavigationPillWidget(),
-              Center(child: Text('Messages', style: Styles.textHeading)),
-              SizedBox(
-                height: 20,
+              GestureDetector(
+                child: ListView(
+                    shrinkWrap: true,
+                    physics: ClampingScrollPhysics(),
+                    children: <Widget>[
+                      NavigationPillWidget(),
+                      Center(
+                          child: Text('Messages', style: Styles.textHeading)),
+                      SizedBox(
+                        height: 20,
+                      ),
+                    ]),
+                onVerticalDragEnd: (details) {
+                  print('Dragged Down');
+                  if (details.primaryVelocity > 50) {
+                    Navigator.pop(context);
+                  }
+                },
               ),
               ListView.separated(
                 shrinkWrap: true,
                 physics: ClampingScrollPhysics(),
                 itemCount: 5,
                 separatorBuilder: (context, index) => Padding(
-                    padding: EdgeInsets.only(left: 75,right: 20),
+                    padding: EdgeInsets.only(left: 75, right: 20),
                     child: Divider(
                       color: Palette.accentColor,
                     )),
